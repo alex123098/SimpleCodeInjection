a80501a4761c2aa11c5448c791815d6d0df7ed71
alex <alex.zaychenko0@gmail.com>
2015-02-19 22:44:52 +0200

    Improved dynamic proxy generation

--- CodeInjection.Tests/LogicInjectorTests.cs
+++ CodeInjection.Tests/LogicInjectorTests.cs
@@ -11,7 +11,7 @@ namespace CodeInjection.Tests
 			ILogicInjector injector = new LogicInjector();
 			var testInstance = new Mock<IInjectionTest>();
 			var pipelineMock = new Mock<IInjectedPipeline>();
-		
+
 			IInjectionTest instance = injector.CreateProxyFor(testInstance.Object, pipelineMock.Object);
 
 			instance.TestVoidMethod();
@@ -32,10 +32,62 @@ namespace CodeInjection.Tests
 
 			pipelineMock.Verify();
 		}
+
+		[Fact]
+		public void CreateProxy_CreatedProxyWillActivatePostExecutePipeline() {
+			ILogicInjector injector = new LogicInjector();
+			var testInstance = new Mock<IInjectionTest>();
+			var pipelineMock = new Mock<IInjectedPipeline>();
+			pipelineMock
+				.Setup(p => p.ExecutePostCondition(It.IsAny<object>(), It.IsAny<MethodInfo>(), It.Is<object[]>(a => a.Length == 0)))
+				.Verifiable();
+
+			IInjectionTest instance = injector.CreateProxyFor(testInstance.Object, pipelineMock.Object);
+
+			instance.TestVoidMethod();
+
+			pipelineMock.Verify();
+		}
+
+		[Fact]
+		public void CreateProxy_CreatedProxyWillCallActualMethod() {
+			ILogicInjector injector = new LogicInjector();
+			var testInstance = new Mock<IInjectionTest>();
+			var pipelineMock = new Mock<IInjectedPipeline>();
+			testInstance
+				.Setup(i => i.TestVoidMethod())
+				.Verifiable();
+
+			IInjectionTest instance = injector.CreateProxyFor(testInstance.Object, pipelineMock.Object);
+
+			instance.TestVoidMethod();
+
+			testInstance.Verify();
+		}
+
+		[Fact]
+		public void CreateProxy_InvokationOfMethodWithArgs_DoesNotThrow() {
+			ILogicInjector injector = new LogicInjector();
+			var testInstance = new Mock<IInjectionTest>();
+			var pipelineMock = new Mock<IInjectedPipeline>();
+
+			IInjectionTest instance = injector.CreateProxyFor(testInstance.Object, pipelineMock.Object);
+
+			instance.TestVoidWithArgs(string.Empty, 0, SimpleEnum.One);
+		}
+	}
+
+
+	public enum SimpleEnum
+	{
+		One,
+		Two
 	}
 
 	public interface IInjectionTest
 	{
 		void TestVoidMethod();
+
+		void TestVoidWithArgs(string stArg, int intArg, SimpleEnum enumArg);
 	}
 }
--- CodeInjection/ProxyBuilder.cs
+++ CodeInjection/ProxyBuilder.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection;
@@ -23,7 +21,9 @@ namespace CodeInjection
 		}
 
 		public Type BuildProxyType(ModuleBuilder moduleBuilder) {
+
 			Contract.Requires(moduleBuilder != null);
+
 			var typeAttributes = CreateProxyTypeAttributes();
 			var typeName = GenerateTypeName();
 			var parentType = typeof(object);
@@ -64,7 +64,12 @@ namespace CodeInjection
 
 		#region Emit methods
 
-		private void EmitInterfaceImplementation(Type @interface, TypeBuilder typeBuilder, FieldBuilder pipelineField, FieldBuilder realInstanceField) {
+		private void EmitInterfaceImplementation(
+			Type @interface, 
+			TypeBuilder typeBuilder, 
+			FieldBuilder pipelineField, 
+			FieldBuilder realInstanceField) {
+
 			var interfaceMethods = @interface.GetMethods();
 			for (var i = 0; i < interfaceMethods.Length; i++) {
 				var method = interfaceMethods[i];
@@ -96,6 +101,13 @@ namespace CodeInjection
 			FieldInfo pipelineField,
 			FieldInfo realInstanceField,
 			MethodInfo methodInfo) {
+
+			Contract.Requires(interfaceName != null);
+			Contract.Requires(il != null);
+			Contract.Requires(pipelineField != null);
+			Contract.Requires(realInstanceField != null);
+			Contract.Requires(methodInfo != null);
+			Contract.Requires(methodIndex > -1);
 			
 			// declare array of parameters for a method
 			var methodParameters = methodInfo.GetParameters();
@@ -113,10 +125,10 @@ namespace CodeInjection
 			il.Emit(OpCodes.Stloc_0);
 
 			// store all parameters
-			for (var i = 1; i <= methodParameters.Length; i++) {
+			for (var i = 0; i < methodParameters.Length; i++) {
 				il.Emit(OpCodes.Ldloc_0);
-				il.Emit(OpCodes.Ldc_I4, i - 1);
-				il.Emit(OpCodes.Ldarg, i);
+				il.Emit(OpCodes.Ldc_I4, i);
+				il.Emit(OpCodes.Ldarg, i + 1);
 				// box if it is a value type
 				if (methodParameters[i].ParameterType.IsValueType) {
 					il.Emit(OpCodes.Box, methodParameters[i].ParameterType);
@@ -125,7 +137,9 @@ namespace CodeInjection
 			}
 
 			// call the PreExecute method
+			il.Emit(OpCodes.Ldarg_0);
 			il.Emit(OpCodes.Ldfld, pipelineField);
+			il.Emit(OpCodes.Ldarg_0);
 			il.Emit(OpCodes.Ldfld, realInstanceField);
 			// call MethodMetadataContainer.GetMethodInfo. Unfortunatelly this is the only way to load method info to stack :(
 			il.Emit(OpCodes.Ldstr, interfaceName);
@@ -136,8 +150,9 @@ namespace CodeInjection
 			il.Emit(OpCodes.Callvirt, typeof(IInjectedPipeline).GetMethod("ExecutePreCondition"));
 
 			// call actual method
+			il.Emit(OpCodes.Ldarg_0);
 			il.Emit(OpCodes.Ldfld, realInstanceField);
-			for (var i = 0; i < methodParameters.Length; i++) {
+			for (var i = 1; i <= methodParameters.Length; i++) {
 				il.Emit(OpCodes.Ldarg, i);
 			}
 			il.Emit(OpCodes.Callvirt, methodInfo);
@@ -148,7 +163,9 @@ namespace CodeInjection
 			}
 
 			// call PostExecute method
+			il.Emit(OpCodes.Ldarg_0);
 			il.Emit(OpCodes.Ldfld, pipelineField);
+			il.Emit(OpCodes.Ldarg_0);
 			il.Emit(OpCodes.Ldfld, realInstanceField);
 			// call MethodMetadataContainer.GetMethodInfo. Unfortunatelly this is the only way to load method info to stack :(
 			il.Emit(OpCodes.Ldstr, interfaceName);
@@ -166,7 +183,15 @@ namespace CodeInjection
 			il.Emit(OpCodes.Ret);
 		}
 
-		private void EmitConstructor(ILGenerator il, FieldInfo pipelineField, FieldInfo realInstanceField) {
+		private void EmitConstructor(
+			ILGenerator il,
+			FieldInfo pipelineField,
+			FieldInfo realInstanceField) {
+
+			Contract.Requires(il != null);
+			Contract.Requires(pipelineField != null);
+			Contract.Requires(realInstanceField != null);
+
 			var parentConstructor = typeof(object).GetConstructor(Type.EmptyTypes);
 			Contract.Assume(parentConstructor != null);
 
@@ -198,7 +223,7 @@ namespace CodeInjection
 		}
 
 		private string GenerateTypeName() {
-			return string.Concat(typeof(T).FullName, Guid.NewGuid().ToString(), "Proxy");
+			return string.Concat(typeof(T).FullName, Guid.NewGuid().ToString("N"), "Proxy");
 		}
 
 		private TypeAttributes CreateProxyTypeAttributes() {
@@ -220,12 +245,14 @@ namespace CodeInjection
 
 		public static void AddMethods(Type type) {
 			Contract.Requires(type != null);
+
 			_types.TryAdd(type.FullName, type);
 		}
 
 		public static MethodInfo GetMethodInfo(string interfaceName, int methodIndex) {
 			Contract.Requires(interfaceName != null);
 			Contract.Requires(methodIndex > -1);
+
 			Type type;
 			if (_types.TryGetValue(interfaceName, out type)) {
 				var methods = type.GetMethods();
