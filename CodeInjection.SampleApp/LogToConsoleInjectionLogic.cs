3abaf40b149cd6e82fbb840d9f221d165676f49f
ozaych <oleksiy_zaychenko@affymetrix.com>
2015-02-24 19:07:12 +0200

    Added sample project

---
+++ CodeInjection.SampleApp/LogToConsoleInjectionLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace CodeInjection.SampleApp
+{
+	internal class LogToConsoleInjectionLogic : IInjectedLogic
+	{
+		public IInjectedLogic BeforeExecute(object target, MethodInfo invokedMethod, object[] parameters, IInjectedLogic next) {
+			var targetType = target.GetType();
+			
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("[Before execute] Starts For:");
+			Console.WriteLine("Type [{0}], instance hash: {{{1}}}", targetType.FullName, target.GetHashCode());
+			Console.WriteLine("Will execute method [{0}]", invokedMethod.Name);
+			Console.WriteLine();
+			Console.ResetColor();
+
+			return next;
+		}
+
+		public IInjectedLogic AfterExecute(object target, MethodInfo invokedMethod, object[] parameters, IInjectedLogic previous) {
+			var targetType = target.GetType();
+
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine();
+			Console.WriteLine("[Before execute] Ends For:");
+			Console.WriteLine("Type [{0}], instance hash: {{{1}}}", targetType.FullName, target.GetHashCode());
+			Console.WriteLine("Method [{0}] execution finished", invokedMethod.Name);
+			Console.ResetColor();
+
+			return previous;
+		}
+	}
+}
---
+++ CodeInjection.SampleApp/SampleService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeInjection.SampleApp
+{
+	public interface ISampleService
+	{
+		void DoSomethingUseful(string clientName);
+	}
+
+	public class SampleService : ISampleService
+	{
+		public void DoSomethingUseful(string clientName) {
+			Console.WriteLine("Did something useful for {0}", clientName);
+		}
+	}
+}
